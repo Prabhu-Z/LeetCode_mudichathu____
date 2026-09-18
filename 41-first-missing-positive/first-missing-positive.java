2cb362441f9a5f6e4f81c74377dbad8cb4eba55d
Prabhu.A <prabhu.a0306@gmail.com>
2026-09-18 14:59:54 +0530

    Time: 19 ms (8.74%) | Memory: 74 MB (26.55%) - LeetSync

--- 41-first-missing-positive/first-missing-positive.java
+++ 41-first-missing-positive/first-missing-positive.java
@@ -5,18 +5,19 @@ class Solution {
 
         int pred = 1;
 
-        for(int i = 0; i < nums.length; i++) {
-
-            if(nums[i] <= 0) {
+        for(int i=0;i<nums.length;i++) {
+            
+            if(nums[i]<=0){
                 continue;
             }
-
-            if(nums[i] == pred) {
+            if(nums[i]==pred){
                 pred++;
             }
-            else if(nums[i] > pred) {
+            else if(nums[i]>pred){
                 return pred;
             }
+
+            
         }
 
         return pred;
